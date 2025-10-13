247f21c3a1a2a32cecac257f0d3a974f41cd8280
Edith Wanja <edithwanjohi254@gmail.com>
2025-10-13 12:11:09 +0300

    feature:Integrate Axios for fetching random recipes and add search bar with filters(cuisine)

--- src/pages/BrowseRecipes.jsx
+++ src/pages/BrowseRecipes.jsx
@@ -1,21 +1,49 @@
 import React, { useState, useEffect } from "react";
-import fetchRecipes from "../utils/api";
+import { getRandomRecipes } from "../utils/api";
+import { getFilteredRecipes } from "../utils/api";
 
 function BrowseRecipes() {
-    const [recipes, setRecipes] = useState([])
-    const [selectedRecipe, setSelectedRecipe] = useState(null)
+  const [recipes, setRecipes] = useState([])
+  const [query, setQuery] = useState("")
+  const [cuisine, setCuisine] = useState("")
+  const [diet, setDiet] = useState("")
+   const [intolerance, setIntolerance] =useState("")
 
   useEffect(() => {
-    const getRecipes = async () => {
-      const data = await fetchRecipes();
+    const loadRecipes = async () => {
+      const data = await getRandomRecipes();
       setRecipes(data);
     };
-    getRecipes();
+    loadRecipes();
   }, []);
    
     return (
-         <div className="px-6 py-10">
+      <div className="px-6 py-10">
       <h2 className="text-3xl font-bold text-center mb-8">Browse Recipes</h2>
+        <form
+        className="flex flex-col md:flex-row items-center justify-center gap-4 mb-10 bg-white p-4 rounded-lg shadow-md"
+      >
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none"
+          />
+          <select
+          value={cuisine}
+          onChange={(e) => setCuisine(e.target.value)}
+          className="border border-gray-300 px-4 py-2 rounded-md w-full md:w-1/5"
+        >
+          <option value="">Cuisine</option>
+          <option value="African">Italian</option>
+          <option value="Mexican">Mexican</option>
+          <option value="Chinese">Chinese</option>
+          <option value="Indian">Indian</option>
+          <option value="American">French</option>
+          <option value="Italian">Chinese</option>
+        </select>
+          </form>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         {recipes.map((recipe) => (
           <div
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,42 @@
-const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY
+import axios from "axios";
 
-const fetchRecipes = async () =>{
-  try {
-    const res = await fetch(
-      `https://api.spoonacular.com/recipes/random?number=20&apiKey=${apiKey}`
-    );
-    if (!res.ok) {
-      throw new Error("Failed to fetch recipes");
-    }
-    const data = await res.json();
-    return data.recipes; 
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-    return [];
-  }
-}
+const API_KEY = import.meta.env.VITE_SPOONACULAR_API_KEY
+const BASE_URL = 'https://api.spoonacular.com/recipes';
 
-export default fetchRecipes
+
+    export const getRandomRecipes = async (number = 20) => {
+        try {
+            const res = await axios.get(`${BASE_URL}/random`, {
+                params: {
+                    number,
+                    apiKey: API_KEY,
+                },
+            });
+            return res.data.recipes; 
+        } catch (error) {
+            console.error("Error fetching random recipes:", error);
+            return [];
+        }
+    };
+
+    
+    export const  getFilteredRecipes = async ({ query, cuisine, diet, intolerances }) => {
+        try {
+            const res = await axios.get(`${BASE_URL}/complexSearch`, {
+                params: {
+                    apiKey: API_KEY,
+                    query: query || '',
+                    cuisine: cuisine || '',
+                    diet: diet || '',
+                    intolerances: intolerances || '',
+                    addRecipeInformation: true,
+                    number: 15,
+                },
+            });
+            return res.data.results;
+        } catch (error) {
+            console.error("Error fetching filtered recipes:", error);
+            return [];
+        }
+    };
+
